6dfb08e1d56085a8536df143b20ebfedde407f59
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-11-11 21:56:42 +0530

    Project Setup

---
+++ src/main/java/com/codelabs/SpringBootCustomeValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.codelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCustomeValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCustomeValidatorApplication.class, args);
+	}
+
+}
