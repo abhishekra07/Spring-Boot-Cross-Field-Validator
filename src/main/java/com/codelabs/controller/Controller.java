25316ab8a180e4e1645412a58e64e0f25516ad93
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-11-11 23:12:50 +0530

    Update Controller.java

--- src/main/java/com/codelabs/controller/Controller.java
+++ src/main/java/com/codelabs/controller/Controller.java
@@ -1,9 +1,11 @@
 package com.codelabs.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codelabs.request.UserValidation;
 import com.codelabs.validator.RequestValidator;
 
 @RestController
@@ -13,4 +15,7 @@ public class Controller {
 	@Autowired
 	RequestValidator validator;
 	
+	public String validateInputs(@RequestBody UserValidation userInput) {
+		
+	}
 }
