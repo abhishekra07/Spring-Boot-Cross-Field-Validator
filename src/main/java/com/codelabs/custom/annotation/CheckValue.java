2f85d3f874590522507aac19be235b46aed5b646
abhishekra07 <40576705+abhishekra07@users.noreply.github.com>
2020-11-12 09:14:47 +0530

    Updated Custom Annotation

--- src/main/java/com/codelabs/custom/annotation/CheckValue.java
+++ src/main/java/com/codelabs/custom/annotation/CheckValue.java
@@ -6,13 +6,19 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.validation.Constraint;
 import javax.validation.Payload;
+
+import com.codelabs.custom.validator.CheckValueValidator;
 
 @Documented
 @Target( { ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = CheckValueValidator.class)
 public @interface CheckValue {
-	String message() default "Invalid value";
+	String message() default "Value is not valid";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+    String errorKey();
+    String fieldType();
 }
--- src/main/java/com/codelabs/custom/annotation/CrossField.java
+++ src/main/java/com/codelabs/custom/annotation/CrossField.java
@@ -6,13 +6,35 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+import javax.validation.Constraint;
 import javax.validation.Payload;
 
+import com.codelabs.custom.validator.CrossFieldValidator;
+
 @Documented
-@Target( { ElementType.METHOD, ElementType.FIELD })
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = CrossFieldValidator.class)
 public @interface CrossField {
 	String message() default "Invalid field";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+    
+    String fieldToValiate() default "";
+    String fieldToValiateAgainst() default "";
+    
+    String code();
+    
+    String errorKey();
+    
+    Class<?> objectToValidate() default Object.class;
+    
+    Class<?> objectToValidateAgainst() default Object.class;
+    
+    @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List{
+    	CrossField[] value();
+    }
 }
